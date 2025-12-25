9e5b9559c350bdea5119ac12bf0ae3d06ff3f173
ZR233 <34859362+ZR233@users.noreply.github.com>
2025-12-25 12:55:07 +0000

    Deploying to gh-pages from @ arceos-hypervisor/axvmconfig@0001f2f34e8438d9dc2971f3a27d53e2ef60a778 🚀

---
+++ trait.impl/core/cmp/trait.Eq.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["axvmconfig",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"axvmconfig/enum.EmulatedDeviceType.html\" title=\"enum axvmconfig::EmulatedDeviceType\">EmulatedDeviceType</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"axvmconfig/enum.VMInterruptMode.html\" title=\"enum axvmconfig::VMInterruptMode\">VMInterruptMode</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"axvmconfig/enum.VMType.html\" title=\"enum axvmconfig::VMType\">VMType</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"axvmconfig/enum.VmMemMappingType.html\" title=\"enum axvmconfig::VmMemMappingType\">VmMemMappingType</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1057]}
---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["axvmconfig",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"axvmconfig/enum.VMType.html\" title=\"enum axvmconfig::VMType\">VMType</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"enum\" href=\"axvmconfig/enum.VMType.html\" title=\"enum axvmconfig::VMType\">VMType</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[734]}
